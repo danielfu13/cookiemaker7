9cb7109f672a9f357c13c96081c7d2041d403226
danielfu13 <daniel.fu123@hotmail.com>
2022-06-15 19:08:09 -0400

    final

--- index.js
+++ index.js
@@ -1,15 +1,89 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generate = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Which license did you use for this project?",
+        choices: [
+          "Apache 2.0",
+          "BSD 3-Clause License",
+          "BSD 2-Clause License",
+          "GNU GPL v3",
+          "IBM",
+          "Boost",
+        ],
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "What is the description of your project?"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "How to install project?"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "How is this project used?"
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message: "Who were the contributors for this project?"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "How would you test this project?"
+    },
+    {
+        type: "input",
+        name: "userName",
+        message: "What is your GitHub username?",
+      },
+      {
+        type: "input",
+        name: "userEmail",
+        message: "What is your email?",
+      },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(`./dist/generate-README.md`, data, err => {
+        if (err) {
+            throw err
+        };
+        console.log('README created!')
+    });
+};
 
-// TODO: Create a function to initialize app
-function init() {}
 
-// Function call to initialize app
-init();
+// function to initialize program
+function init() {
+    return inquirer.prompt(questions);
+};
+
+// function call to initialize program
+init()
+.then(userInput => {
+    return generate(userInput);
+})
+.then(readmeInfo => {
+    return writeToFile(readmeInfo);
+})
+.catch(err => {
+    console.log(err);
+})
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,86 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { 
+  if (license !== 'no license') {
+    return `
+  ![badge](https://img.shields.io/badge/license-${license}-blue)
+    `;
+  } else {
+    return ' ';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'no license') {
+  return `
+  [${license}](https://choosealicense.com/licenses/${license})
+    `;
+  } else {
+    return ' ';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'no license') {
+  return `
+  ## License
+  The application is covered under the following license:
+  ${renderLicenseLink(license)}
+    `;
+  } else {
+    return ' ';
+  }
+ }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  
+  # ${data.title}
+
+  ## Table of contents
+  - [Description](#Description)
+  - [Installation](#Installation)
+  - [Usage](#Usage)
+  - [Licence](#Licence)
+  - [Contributors](#Contributors)
+  - [Test](#Test)
+  - [Repository Link](#Repository)
+  - [GitHub Info](#GitHub) 
+
+  ## Project Description
+
+  ${data.description}
+
+  ## Installation 
+
+  ${data.installation}
+  
+  ## Usage
+
+  ${data.usage}
+
+  ## Licence
+
+  ${data.licence}
+
+  ${renderLicenseSection(data.license)}
+
+  ## Contributors
+
+  ${data.contribution}
+
+  ## Test
+
+  ${data.test}
+
+  ## Contact Information:
+  * GitHub Username: ${data.userName}
+  * GitHub Email: ${data.userEmail}
 
 `;
 }
